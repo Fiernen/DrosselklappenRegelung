7069d060f261b2df15b2e31319322b02aa37e3d2
Fiernen <33194365+Fiernen@users.noreply.github.com>
2022-05-12 11:09:54 +0200

    inital Commig Code Project

---
+++ Code/main.c
@@ -0,0 +1,18 @@
+/*
+ * DrosselklappenRegelung.c
+ *
+ * Created: 12.05.2022 10:27:16
+ * Author : beatf
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+

ded111d80caba518973d75ebd5aa9efa99d903dd
Fiernen <33194365+Fiernen@users.noreply.github.com>
2022-05-30 17:17:44 +0200

    in Funktionen aufgeteilt

--- Code/main.c
+++ Code/main.c
@@ -1,46 +1,18 @@
-#define	F_CPU			3686400
+#define	F_CPU 3686400
+
 #include <avr/interrupt.h>
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define FILTER_ORDER 5
-uint16_t filter_container[FILTER_ORDER];
 
-uint16_t AD_value_2; 
-uint16_t AD_value_5;
-uint16_t position;	// Mean AD value
-
-int16_t speed;
-int16_t speed_setpoint;
-int16_t speed_P_term; // Result for P-term
-int16_t speed_I_term;
-int16_t prev_time_step_position = 0;
-// Vars for control loop:
-
-// P-position-controller:
-int16_t position_setpoint = 863;
-int16_t position_error;
 int16_t kP_position = 1; // Gain
-
-
-
-// D-Term:
-	
-// PI-speed-controller:
-
-int16_t speed_error;
-	
-// P-term:
 int16_t kP_speed = 1; // Gain
-
-// I-term:
 int16_t TN_speed = 5; // Integrator time constant
-int16_t speed_error_integral = 0;
-int32_t temp_integral;
+uint16_t position;	// Mean AD value
+int16_t position_setpoint = 863;
 
-// Result
-int32_t temp_PWM_duty_cycle; // temp value to check for overflow
-int16_t PWM_duty_cycle;
+
+int16_t duty_cycle;
 
 
 
@@ -273,82 +245,24 @@ void TimerPWM_init(void)
 
 
 
-int main(void)
-{
-	#if DEBUG
-	DDRB |= 1<<DDB0;
-	#endif
-	
-	
-	char lcd_str[16];
-	
-	// Initialization:
-	lcd_init();
-	USART_init();
-	TimerPWM_init();
-	
-	OCR1A = 0x07FF*0.50; // Update PWM timer compare value
-	
-	// Init Timer 0 for measuring and PID Calculation:
-	TCCR0 = (1<<CS01)|(1<<CS00);	// clk_IO/64 --> sample rate 4.42 ms
-	TIMSK |= (1<<TOIE0);			// Enable interrupt for Timer overflow	
-	
-    // Init ADC:
-	ADMUX = (0<<REFS1) | (1<<REFS0); // Internal voltage reference
-	ADMUX |= 0<<ADLAR; // write right sided
-	ADCSRA |= 1<<ADEN; // Enable
-	ADCSRA |=  (1<<ADPS2) | (1<<ADPS1) | (0<<ADPS0); // Prescaler = 64
-	
-	
-// 	struct filter_param{
-// 		final uint8_t order 5
-// 		
-// 		};
-	
-	sei();
-	
-	while(1)
-	{
-		lcd_cmd(0xC1);
-		lcd_zahl_16(OCR1A,lcd_str);
-		lcd_text(lcd_str);
-		USART_send_16(position);
-		
-		lcd_cmd(0xC7);
-		lcd_zahl_s16(PWM_duty_cycle,lcd_str);
-		lcd_text(lcd_str);
-
-		lcd_cmd(0x80);
-		lcd_zahl_s16(speed_P_term,lcd_str);
-		lcd_text(lcd_str);
-
-		lcd_cmd(0x87);
-		lcd_zahl_s16(speed_I_term,lcd_str);
-		lcd_text(lcd_str);
-
-	}
-	
-}
-
-
-
 /* measure() measueres the AD-values at C2 and C5 and means the values
 
 */
 int16_t position_measure(void)
 {
-	// Minima and maxima for both 
+	
 	#define LOW_ADC2 172+1-74-2
 	#define HIGH_ADC2 960
 	#define LOW_ADC5 843+3+76+1
 	#define HIGH_ADC5 053
+
 	// ADC2
 	ADMUX &= ~0b1111;
 	ADMUX |= 0b0010; // PC2
 	// Measure
 	ADCSRA |= 1<<ADSC; // Start Conversion
 	while(ADCSRA&(1<<ADSC)); // Wait for completed conversion (ADSC switches back to 0)
-	AD_value_2 = ADC;
+	uint16_t AD_value_2 = ADC;
 	AD_value_2 -= LOW_ADC2;
 	
 	// ADC5
@@ -357,7 +271,7 @@ int16_t position_measure(void)
 	// Measure
 	ADCSRA |= 1<<ADSC; // Start Conversion
 	while(ADCSRA&(1<<ADSC)); // Wait for completed conversion (ADSC switches back to 0)
-	AD_value_5 = ADC;
+	uint16_t AD_value_5 = ADC;
 	AD_value_5 = -AD_value_5 + LOW_ADC5;
 	
 	// Open-circuit detection of the potentiometers:
@@ -369,15 +283,16 @@ int16_t position_measure(void)
 }
 
 
+
 struct filter_params {
 	#define filter_size 5
-	int16_t container[filter_size] = {0};
-	uint8_t increment = 0;
-	uint8_t last_increment = filter_size;
-	int16_t sum = 0;
+	int16_t container[filter_size];
+	uint8_t increment;
+	uint8_t last_increment;
+	int16_t sum;
 };
 
-// filter = 
+struct filter_params filter;
 
 int16_t FIR_filter(int16_t new_value, struct filter_params *params)
 {
@@ -395,26 +310,22 @@ int16_t FIR_filter(int16_t new_value, struct filter_params *params)
 	return params->sum/filter_size; // Return mean value
 }
 
-int16_t motor_control(int16_t position, int)
+// struct controller_params {
+// 	int16_t MAX_position_error = INT16_MAX/kP_position;
+// 	int16_t MIN_position_error = -INT16_MAX/kP_position;
+// 	int16_t MAX_speed_error = INT16_MAX/kP_speed;
+// 	int16_t MIN_speed_error = -INT16_MAX/kP_speed;
+// 	int16_t MAX_speed_error_integral = 0x6FFF/5;
+// 	int16_t MIN_speed_error_integral = -0x6FFF/5;
+// 	#define MAX_PWM_duty_cycle INT16_MAX
+// 	#define MIN_PWM_duty_cycle -INT16_MAX // must be symetrical for scaling
+// 	};
+
+int16_t prev_time_step_position = 0;
+int16_t speed_error_integral = 0;
+
+int16_t Motor_controller()
 {
-	
-}
-
-ISR(TIMER0_OVF_vect) // Last runtime measure = 0.483 us
-{
-	/* ISR(TIMER0_OVF_vect) is a interrupt which is triggered by a timer 0 overflow.
-		
-	
-	*/
-	#if DEBUG
-	PORTB |= 1<<0; // Time measure 
-	#endif
-	
-
-	
-	position = position_measure();
-
-
 	// Motor control:
 	/* Cascading P-position-controller into PI-speed-controller
 	
@@ -422,7 +333,7 @@ ISR(TIMER0_OVF_vect) // Last runtime measure = 0.483 us
 		Filter		--> revolving vector
 		Derivative	--> slope from last sample point
 		
-	 */
+	*/
 	
 	// Limits for overflow protection
 	int16_t MAX_position_error = INT16_MAX/kP_position;
@@ -434,12 +345,13 @@ ISR(TIMER0_OVF_vect) // Last runtime measure = 0.483 us
 	#define MAX_PWM_duty_cycle INT16_MAX
 	#define MIN_PWM_duty_cycle -INT16_MAX // must be symetrical for scaling
 	
+	
 	// D-Term-speed;
-	speed = (position-prev_time_step_position); // derivative
+	int16_t speed = (position-prev_time_step_position); // derivative
 	prev_time_step_position = position;
 
 	// P-term-position, with overflow protection:
-	position_error = position_setpoint - position;
+	int32_t position_error = position_setpoint - position;
 	if (position_error > MAX_position_error)
 	{
 		position_error = MAX_position_error;
@@ -448,10 +360,10 @@ ISR(TIMER0_OVF_vect) // Last runtime measure = 0.483 us
 	{
 		position_error = MIN_position_error;
 	}
-	speed_setpoint = kP_position * position_error;
+	int16_t speed_setpoint = kP_position * position_error;
 	
 	// P-term-speed, with overflow protection:
-	speed_error = speed_setpoint - speed;
+	int32_t speed_error = speed_setpoint - speed;
 	if (speed_error > MAX_speed_error)
 	{
 		speed_error = MAX_speed_error;
@@ -460,10 +372,10 @@ ISR(TIMER0_OVF_vect) // Last runtime measure = 0.483 us
 	{
 		speed_error = MIN_speed_error;
 	}
-	speed_P_term = kP_speed * speed_error;
+	int16_t speed_P_term = kP_speed * speed_error;
 	
 	// I-term-speed, with limits/overflow protection:
-	temp_integral = speed_error_integral + speed_P_term;
+	int32_t temp_integral = speed_error_integral + speed_P_term;
 	if (temp_integral > MAX_speed_error_integral)
 	{
 		speed_error_integral = MAX_speed_error_integral;
@@ -474,29 +386,122 @@ ISR(TIMER0_OVF_vect) // Last runtime measure = 0.483 us
 	}
 	else
 	{
-		speed_error_integral = speed_error_integral + speed_error;
+		speed_error_integral = speed_error_integral + speed_P_term;
 		
 	}
-	speed_I_term = speed_error_integral/TN_speed;
+	int16_t speed_I_term = speed_error_integral/TN_speed;
 	
 	// Control value sum, with limits/overflow protection:
-	temp_PWM_duty_cycle = speed_P_term + speed_I_term;
+	int32_t duty_cycle;
+	int32_t temp_PWM_duty_cycle = speed_P_term + speed_I_term;
 	if (temp_PWM_duty_cycle > MAX_PWM_duty_cycle)
 	{
-		PWM_duty_cycle = MAX_PWM_duty_cycle;
+		duty_cycle = MAX_PWM_duty_cycle;
 	}
 	else if(temp_PWM_duty_cycle < MIN_PWM_duty_cycle)
 	{
-		PWM_duty_cycle = MIN_PWM_duty_cycle;
+		duty_cycle = MIN_PWM_duty_cycle;
 	}
 	else
 	{
-		PWM_duty_cycle = speed_P_term + speed_I_term;
+		duty_cycle = speed_P_term + speed_I_term;
 	}	
 	
 	// PWM scaling:
-	PWM_duty_cycle /= MAX_PWM_duty_cycle/0x7FF;
-	OCR1A = 0x7FF/2 + PWM_duty_cycle;
+	duty_cycle /= MAX_PWM_duty_cycle/0x7FF;
+	return duty_cycle;
+}
+
+
+
+/* TimerController_init() initialises the Timer/Counter 1 for controller interrupt
+	Timer 0 for measuring and PID Calculation:
+*/
+void TimerController_init()
+{
+	TCCR0 = (1<<CS01)|(1<<CS00);	// clk_IO/64 --> sample rate 4.42 ms / sample freq 225 Hz
+	TIMSK |= (1<<TOIE0);			// Enable interrupt for Timer overflow		
+}
+
+/* ADConverter_init() does the setup for the AD-Converter
+
+*/
+void ADConverter_init()
+{
+	ADMUX = (0<<REFS1) | (1<<REFS0); // Internal voltage reference
+	ADMUX |= 0<<ADLAR; // write right sided
+	ADCSRA |= 1<<ADEN; // Enable
+	ADCSRA |=  (1<<ADPS2) | (1<<ADPS1) | (0<<ADPS0); // Prescaler = 64
+	
+}
+
+int main(void)
+{
+	#if DEBUG
+	DDRB |= 1<<DDB0;
+	#endif
+		
+	char lcd_str[16];
+	
+	// Initialization:
+	lcd_init();
+	USART_init();
+	TimerPWM_init();
+	OCR1A = ICR1*0.50; // Update PWM timer compare value
+	TimerController_init();
+	ADConverter_init();
+	sei();
+	
+	// Main Loop:
+	while(1)
+	{
+		// Write to LCD-display:
+		lcd_cmd(0xC1);
+		lcd_zahl_16(OCR1A,lcd_str);
+		lcd_text(lcd_str);
+		
+		lcd_cmd(0xC7);
+		lcd_zahl_s16(duty_cycle,lcd_str);
+		lcd_text(lcd_str);
+
+// 		lcd_cmd(0x80);
+// 		lcd_zahl_s16(speed_P_term,lcd_str);
+// 		lcd_text(lcd_str);
+// 
+// 		lcd_cmd(0x87);
+// 		lcd_zahl_s16(speed_I_term,lcd_str);
+// 		lcd_text(lcd_str);
+
+		// Send over USART:
+		USART_send_16(position);
+		
+		// Catch new controller parameters:
+
+	}
+	
+}
+
+
+/* ISR(TIMER0_OVF_vect) is a interrupt which is triggered by a timer 0 overflow.
+
+
+*/
+ISR(TIMER0_OVF_vect)
+{
+
+	#if DEBUG
+	PORTB |= 1<<0; // Time measure 
+	#endif
+	
+
+	
+	position = position_measure();
+	
+	position = FIR_filter(position, &filter);
+
+	duty_cycle = Motor_controller();
+	
+	OCR1A = ICR1/2 + duty_cycle;
 	
 	
 	

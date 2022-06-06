6bceca65f45e7243bfe72461023bbea2752e8b92
Fiernen <33194365+Fiernen@users.noreply.github.com>
2022-06-06 10:50:48 +0200

    more fixes

--- Code/lcd_functions.h
+++ Code/lcd_functions.h
@@ -4,7 +4,7 @@
 void lcd_zahl(uint8_t zahl,char* text);
 void lcd_zahl_16(uint16_t num, char* written);
 void lcd_zahl_s16(int16_t num, char* written);
-void lcd_angle(uint16_t num, char* written)
+void lcd_angle(uint16_t num, char* written);
 void lcd_text(char* ztext);
 void lcd_cmd(unsigned char cmd);
 void lcd_init();
